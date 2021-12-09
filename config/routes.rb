fbc058b12a8a6c30b9461cdb94aa21b163cae561
Lucas TCHITDJIAN <tchitdjian.lucas@gmail.com>
2021-12-09 16:58:24 +0100

    End of exercice2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'lists#index'
+  get '/movies', to: 'movies#index'
+  resources :lists do 
+    resources :bookmarks, only: [:index, :new, :create]
+  end
+  resources :bookmarks, only: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# db/seeds.rb
+require 'json'
+require 'open-uri'
+require 'faker'
+
+
+puts 'cleaning .....'
+
+
+Bookmark.destroy_all
+Movie.destroy_all
+List.destroy_all
+
+
+puts 'seeeding......'
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+movies_serialized = URI.open(url).read
+ movies = JSON.parse(movies_serialized)
+movies['results'].each do |movie|
+  title = movie['original_title']
+  overview = movie['overview']
+  poster_url = movie['backdrop_path']
+  rating = movie['vote_average'].to_i
+  Movie.create!(title: title, overview: overview, poster_url: poster_url, rating: rating)
+  puts 'created 1 movie ....'
+end
+puts 'seeding done'
+10.times do
+  l = List.new(name:Faker::Cannabis.strain)
+  l.save
+  puts 'Created one bookmark'
+end
+100.times do
+  b = Bookmark.new(comment: 'Random', movie: Movie.all.sample, list: List.all.sample)
+  b.save
+  puts 'Created one bookmark'
+end
